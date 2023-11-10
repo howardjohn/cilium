63d27743cebafc949009b7fb3fab3cc3110560a9
John Howard <howardjohn@google.com>
2023-11-09 20:27:08 -0600

    v2

--- daemon/cmd/daemon.go
+++ daemon/cmd/daemon.go
@@ -779,6 +779,15 @@ func newDaemon(ctx context.Context, cleaner *daemonCleanup, params *daemonParams
 
 		params.NodeManager.Subscribe(params.WGAgent)
 	}
+	log.Infof("howardjohn: starting")
+	if params.HBONEAgent != nil {
+		log.Infof("howardjohn: starting HBONE agent")
+		if err := params.HBONEAgent.Init(d.ipcache, d.endpointManager); err != nil {
+			log.WithError(err).Error("failed to initialize hbone agent")
+			return nil, nil, fmt.Errorf("failed to initialize hbone agent: %w", err)
+		}
+		log.Infof("howardjohn: started HBONE agent")
+	}
 
 	// Perform an early probe on the underlying kernel on whether BandwidthManager
 	// can be supported or not. This needs to be done before handleNativeDevices()
